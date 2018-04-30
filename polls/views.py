4647ccb00c772ffb4bbf63e08036dd945d4d27b0
Co0oW <b.yang@hotmail.com>
2018-04-30 16:02:29 -0400

    Added Detail, Result, Vote placeholder views

--- polls/views.py
+++ polls/views.py
@@ -15,3 +15,14 @@ def random_generator(request, max_rand):
     random_int = random.randrange(0, int(max_rand))
     message = 'Random number generated between 0 and %s: %d' % (max_rand, random_int)
     return HttpResponse(message)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." %question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
