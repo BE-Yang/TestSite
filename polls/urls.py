f6703ac4beda0500d2d0423858f4d1675884b083
Co0ow <b.yang@hotmail.com>
2018-05-06 22:11:50 -0400

    Added namespace app_names = 'polls'

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^random_number/$', views.random_generator, {'max_rand':100}, name='number generator default'),
