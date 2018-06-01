7608ead7f9cc9177e11cf23abd7233747affd5d5
Co0oW <b.yang@hotmail.com>
2018-06-01 17:24:33 -0400

    Added additional tests to test for questions over 1 day and tests within the day

--- polls/tests.py
+++ polls/tests.py
@@ -16,9 +16,28 @@ class QuestionModelTests(TestCase):
         '''
         Return false if Question was created with publish date in the future
         '''
-        future_time = timezone.now() + datetime.timedelta(days=30)
+
+        future_time = timezone.now() + datetime.timedelta(days = 30)
         future_question = Question(pub_date=future_time)
 
         self.assertIs(future_question.was_recently_published(), False)
 
+    def test_old_dates(self):
+        '''
+        Return False if question was older than 1 day
+        '''
 
+        old_time = timezone.now() - datetime.timedelta(days = 1, seconds = 1)
+        oldQuestion = Question(pub_date = old_time)
+
+        self.assertIs(oldQuestion.was_recently_published(), False)
+
+    def test_same_day(self):
+        '''
+        Return True if question was published within the day
+        '''
+
+        time = timezone.now() - datetime.timedelta(hours = 23, minutes = 59, seconds = 59)
+        recentQuestion = Question(pub_date = time)
+
+        self.assertIs(recentQuestion.was_recently_published(), True)
