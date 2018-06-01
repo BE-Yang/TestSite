811a9a68fbc39698234571580034402da11d7725
Co0oW <b.yang@hotmail.com>
2018-05-31 21:29:04 -0400

    Used Django Generic views for repetitive views

--- polls/views.py
+++ polls/views.py
@@ -5,38 +5,27 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 from django.core.urlresolvers import reverse
+from django.views import generic
 # from django.template import loader
 import random
 # Create your views here.
 
 
-def index(request):
-    latest_questions_list = Question.objects.order_by('pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    context = {'latest_questions_list':latest_questions_list}
-    # message = ', '.join([q.question_text for q in latest_questions_list])
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_questions_list'
 
+    def get_queryset(self):
+        """Returns the last five published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def random_generator(request, max_rand):
-    random_int = random.randrange(0, int(max_rand))
-    message = 'Random number generated between 0 and %s: %d' % (max_rand, random_int)
-    return HttpResponse(message)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    # response = "You're looking at the results of question %s."
-    # return HttpResponse(response % question_id)
-    question = get_object_or_404(Question, pk = question_id)
-    return render(request, 'polls/results.html', {'question':question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -54,3 +43,7 @@ def vote(request, question_id):
 
     return HttpResponse("You're voting on question %s." % question_id)
 
+def random_generator(request, max_rand):
+    random_int = random.randrange(0, int(max_rand))
+    message = 'Random number generated between 0 and %s: %d' % (max_rand, random_int)
+    return HttpResponse(message)
