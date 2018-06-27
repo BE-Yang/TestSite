8020fe612006e320ef23e633d67fead48212b1ff
Co0oW <b.yang@hotmail.com>
2018-06-26 23:47:00 -0400

    Changed Drink class to be abstract for inheritance Corrected spellings and deleted old database to accommodate the new Drink class

--- BartendingRefresher/models.py
+++ BartendingRefresher/models.py
@@ -6,10 +6,43 @@ from django.db import models
 # Create your models here.
 
 class Drink(models.Model):
-    glass = models.CharField(max_length=50)
-    alcohol = models.CharField(max_length=50)
-    garnish = models.CharField(max_length=50)
-    method = models.CharField(max_length=50)
-    type = models.CharField(max_length=50)
+    glass = models.CharField(max_length=50, default="Old Fashion")
+    alcohol = models.CharField(max_length=50, default='Vodka')
+    method = models.CharField(max_length=50, default='Build')
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+
+    class Meta:
+        abstract=True
+
+class Martini(models.Model):
+    pass
+
+class Russian(Drink):
+    glass = models.CharField(max_length=10, default='Rocks')
+    alcohol = 'vodka'
+    method = 'Build'
+    type = 'Russian'
+    garnish = 'Cherry'
 
 
+# class God(Drink):
+#     glass = 'Rocks'
+#     type = 'Build'
+#     garnish = 'None'
+
+
+class Screws(models.Model):
+    pass
+
+class FizzSours(models.Model):
+    pass
+
+
+class Bartender():
+    menu = {'Russian': Russian}
+
+    def MakeDrink(self, type, **kwargs):
+
+        return self.menu[type.capitalize()](**kwargs)
--- TestSite/urls.py
+++ TestSite/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('polls/', include('polls.urls')),
+    url('bargame/', include('BartendingRefresher.urls'))
 ]
