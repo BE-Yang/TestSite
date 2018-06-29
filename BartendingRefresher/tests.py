d118c58feacfd3c3dc9b683c5b286623863cd1e2
Co0oW <b.yang@hotmail.com>
2018-06-28 23:39:28 -0400

    included testing for Russian Drink

--- BartendingRefresher/tests.py
+++ BartendingRefresher/tests.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from BartendingRefresher.models import Drink, Russian, Bartender
 
 # Create your tests here.
+
+
+class RussianInheritanceTest(TestCase):
+
+    def test_Inheritance_Glass(self):
+        Bar = Bartender()
+        Russian = Bar.MakeDrink('Russian', name = 'TestRussian')
+
+        self.assertEqual(Russian.glass, 'Rocks')
