850fca3cb59fc642087229e5c317427c4e613b7a
Co0oW <b.yang@hotmail.com>
2018-07-05 17:03:40 -0400

    Added Foreign Keys and fixed __init__ argument errors as well as related name error

--- BartendingRefresher/models.py
+++ BartendingRefresher/models.py
@@ -5,71 +5,72 @@ from django.db import models
 
 # Create your models here.
 class Ingredient(models.Model):
-    Ingredient = models.CharField(max_length=20, primary_key=True)
-    Type = models.CharField(max_length=10,
-                            choices=[
-                                ('Al', 'Alcohol'),
-                                ('Li', 'Liqueur'),
-                                ('Gr', 'Garnish'),
-                                ('Fill', 'Filler')
-                                ]
-                            )
-    class Meta:
-        managed=True
+	Ingredient = models.CharField(max_length=20, primary_key=True)
+	Type = models.CharField(max_length=10,
+							choices=[
+								('Al', 'Alcohol'),
+								('Li', 'Liqueur'),
+								('Gr', 'Garnish'),
+								('Fill', 'Filler')
+							]
+							)
 
-    def __repr__(self):
-        return '%s, %s' %(self.Ingredient, self.Type)
+	@classmethod
+	def IngredientList(cls, type):
+		return Ingredient.objects.filter(Type=type)
 
-    def __str__(self):
-        return self.Ingredient
+	def __repr__(self):
+		return '%s, %s' %(self.Ingredient, self.Type)
+
+	def __str__(self):
+		return self.Ingredient
 
 class Drink(models.Model):
-    name = models.CharField(max_length=30, primary_key=True, blank=True, default='')
-    glass = models.CharField(max_length=20)
-    method = models.CharField(max_length=10)
-    alcohol = models.CharField(max_length=10)
+	name = models.CharField(max_length=30, primary_key=True, blank=True, default='')
+	glass = models.CharField(max_length=20)
+	method = models.CharField(max_length=10)
+	alcohol = models.CharField(max_length=10)
 
-    class Meta:
-        abstract = True
+	class Meta:
+		abstract = True
 
 #
 # class Martini(Drink):
 #     pass
 
 class Russian(Drink):
-    method = models.CharField(max_length=10, default='Build')
-    glass = models.CharField(max_length=20, default='Rocks')
-    alcohol = models.ForeignKey(Ingredient, related_name='%(class)s_Alcohol',\
-                                on_delete=models.CASCADE, default='Vodka')
-    second_alcohol = models.ForeignKey(Ingredient, related_name='%(class)s_Second',\
-                                       on_delete=models.CASCADE, default='Kahlua',
-                                       limit_choices_to={'Al':True}
-                                       )
-    third_alcohol = models.ForeignKey(Ingredient, related_name='%(class)s_Third',\
-                                      on_delete=models.CASCADE, blank=True, null=True, \
-                                      limit_choices_to={'Al':True}
-                                      )
-    garnish = models.ForeignKey(Ingredient, related_name='%(class)s_Garnish',\
-                                on_delete=models.CASCADE, blank=True, null=True,\
-                                limit_choices_to={'GR':True}
-                                )
-    filler = models.ForeignKey(Ingredient, related_name='%(class)s_Filler', \
-                               on_delete=models.CASCADE, blank=True, null=True,
-                               limit_choices_to={'Fill':True}
-                               )
+	method = models.CharField(max_length=10, default='Build')
+	glass = models.CharField(max_length=20, default='Rocks')
+	alcohol = models.ForeignKey(Ingredient, related_name='%(class)s_Alcohol', \
+								on_delete=models.CASCADE, default='Vodka')
+	second_alcohol = models.ForeignKey(Ingredient, related_name='%(class)s_Second', \
+									   on_delete=models.CASCADE, default='Kahlua',
+									   limit_choices_to=Ingredient.IngredientList('Al')
+									   )
+	third_alcohol = models.ForeignKey(Ingredient, related_name='%(class)s_Third', \
+									  on_delete=models.CASCADE, blank=True, null=True, \
+									  limit_choices_to=Ingredient.IngredientList('Al')
+									  )
+	garnish = models.ForeignKey(Ingredient, related_name='%(class)s_Garnish', \
+								on_delete=models.CASCADE, blank=True, null=True, \
+								limit_choices_to=Ingredient.IngredientList('GR')
+								)
+	filler = models.ForeignKey(Ingredient, related_name='%(class)s_Filler', \
+							   on_delete=models.CASCADE, blank=True, null=True,
+							   limit_choices_to=Ingredient.IngredientList('Fill')
+							   )
 
-    def __init__(self, *args, **kwargs):
-        super(Russian, self).__init__(*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		super(Russian, self).__init__(*args, **kwargs)
 
-    class Meta:
-        managed=True
 
-    def __repr__(self):
-        return '%s, %s, %s, %s, %s, %s' %(self.name, self.method, self.glass, \
-                                          self.alcohol, self.second_alcohol, self.garnish)
 
-    def __str__(self):
-        return self.name
+	def __repr__(self):
+		return '%s, %s, %s, %s, %s, %s' %(self.name, self.method, self.glass, \
+										  self.alcohol, self.second_alcohol, self.garnish)
+
+	def __str__(self):
+		return self.name
 # class God(Drink):
 #     glass = 'Rocks'
 #     type = 'Build'
@@ -83,8 +84,8 @@ class Russian(Drink):
 
 
 class Bartender():
-    menu = {'Russian': Russian}
+	menu = {'Russian': Russian}
 
-    def MakeDrink(self, type, **kwargs):
+	def MakeDrink(self, type, **kwargs):
 
-        return self.menu[type.capitalize()](**kwargs)
+		return self.menu[type.capitalize()](**kwargs)
